aa83b4b39951e48b10e6855ad1fd2d2f2b18b864
carloeusebi <carloeusebi@gmail.com>
2025-08-15 15:31:36 +0200

    wip

---
+++ app/app.config.ts
@@ -0,0 +1,90 @@
+import { ConfigContext, ExpoConfig } from '@expo/config';
+
+const IS_DEV = (process.env.APP_VARIANT = 'development');
+const IS_PREVIEW = (process.env.APP_VARIANT = 'preview');
+
+const getUniqueIdentifier = () => {
+    if (IS_DEV) {
+        return 'com.bestbefore.app.dev';
+    }
+    if (IS_PREVIEW) {
+        return 'com.bestbefore.app.preview';
+    }
+    return 'com.bestbefore.app';
+};
+
+const getAppName = () => {
+    if (IS_DEV) {
+        return 'BestBefore (Dev)';
+    }
+
+    if (IS_PREVIEW) {
+        return 'BestBefore (Preview)';
+    }
+
+    return 'BestBefore';
+};
+
+export default ({ config }: ConfigContext): ExpoConfig => ({
+    ...config,
+    name: getAppName(),
+    slug: 'bestbefore',
+    version: '1.0.0',
+    orientation: 'portrait',
+    icon: './assets/images/icon.png',
+    scheme: 'app',
+    userInterfaceStyle: 'automatic',
+    newArchEnabled: true,
+    ios: {
+        supportsTablet: true,
+        bundleIdentifier: getUniqueIdentifier(),
+    },
+    android: {
+        adaptiveIcon: {
+            foregroundImage: './assets/images/adaptive-icon.png',
+            backgroundColor: '#ffffff',
+        },
+        edgeToEdgeEnabled: true,
+        package: getUniqueIdentifier(),
+        permissions: ['android.permission.CAMERA'],
+    },
+    web: {
+        bundler: 'metro',
+        output: 'static',
+        favicon: './assets/images/favicon.png',
+    },
+    plugins: [
+        'expo-router',
+        [
+            '@react-native-google-signin/google-signin',
+            {
+                iosUrlScheme: 'com.googleusercontent.apps.64881970768-0srddcup1p4joeocn9e0qoler4fasu8r',
+            },
+        ],
+        [
+            'expo-camera',
+            {
+                cameraPermission: "Permetti l'accesso alla fotocamera per $(PRODUCT_NAME)",
+            },
+        ],
+        [
+            'expo-splash-screen',
+            {
+                image: './assets/images/splash-icon.png',
+                imageWidth: 200,
+                resizeMode: 'contain',
+                backgroundColor: '#ffffff',
+            },
+        ],
+    ],
+    experiments: {
+        typedRoutes: true,
+    },
+    extra: {
+        router: {},
+        eas: {
+            projectId: '9c35470a-d4b0-4345-8975-50c0a38cc869',
+        },
+    },
+    owner: 'carloeusebi',
+});
